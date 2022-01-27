6c06882b360f6e0ece1a0184bf61ac308243bc6e
Ser_Yo_Ja <seroja.92@mail.ru>
2022-01-27 20:37:26 +0300

    FrameLayout детально

--- app/src/main/java/ru/gb/myapptestlayout/MainActivity.java
+++ app/src/main/java/ru/gb/myapptestlayout/MainActivity.java
@@ -2,13 +2,26 @@ package ru.gb.myapptestlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSend = findViewById(R.id.send_button);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent duck = new Intent(this, MainActivity2.class);
+                startActivity(duck);
+            }
+        });
     }
 }
